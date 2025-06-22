e2f075a26450f72ea4087c61888905c070ffc999
rei <raihanryuichi@gmail.com>
2025-06-22 17:13:24 +0700

    revert config.def.h to use default value

--- config.def.h
+++ config.def.h
@@ -1,17 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;               /* -b  option; if 0, dmenu appears at bottom     */
-static int vertpad = 12;             /* vertical padding of bar */
-static int sidepad = 12;             /* horizontal padding of bar */
-static int user_bh = 12;             /* add an defined amount of pixels to the bar height */
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int vertpad = 0;                     /* vertical padding of bar */
+static int sidepad = 0;                     /* horizontal padding of bar */
+static int user_bh = 0;                     /* add an defined amount of pixels to the bar height */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	// "Terminus (TTF):pixelsize=12:antialias=true:autohint=true"
-	// "Fixedsys Excelsior:pixelsize=14:antialias=true:autohint=true"
-	// "ProggyCleanTTSZ:size=12:antialias=true:autohint=true"
-	"Cozette:pixelsize=12:antialias=false:autohint=true"
+	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static char normbg[] = "#222222";
